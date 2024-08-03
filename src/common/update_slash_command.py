cb80515126f0a657de6c2b12e3edca8baced94d2
saichann <55883274+sai11121209@users.noreply.github.com>
2024-08-04 05:06:44 +0900

    修正

--- src/common/update_slash_command.py
+++ src/common/update_slash_command.py
@@ -1,5 +1,6 @@
 import os
 import requests as rq
+from pathlib import Path
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APPLICATION_ID = os.environ.get("APPLICATION_ID")
@@ -9,25 +10,27 @@ def lambda_handler(event, context):
     guilds_url = f"{base_url}/users/@me/guilds"
     headers = {
         "Content-Type": "application/json",
-        "User-Agent": "DiscordBot (private use) Python-urllib/3.10",
+        "User-Agent": "DiscordBot (private use) Python-urllib/3.11",
         "Authorization": f"Bot {BOT_TOKEN}",
     }
 
     # ギルドのリストを取得
     response = rq.get(guilds_url, headers=headers)
 
+    commands_directory = Path("src/commands")
     command_list = [
         {
-            'name': 'dog',
-            'description': 'ランダムで犬の画像を返します。',
-            'options': []
+            "name": command_file.stem,
+            "description": "",
+            "options": []
         }
+        for command_file in commands_directory.glob("*.py")
     ]
 
     if response.status_code == 200:
         guilds = response.json()
         for guild in guilds:
-            guild_id = guild['id']
+            guild_id = guild["id"]
             print(f"Processing guild: {guild_id}")
 
             # 既存のコマンドを取得
@@ -36,13 +39,13 @@ def lambda_handler(event, context):
             if existing_commands_response.status_code == 200:
                 existing_commands = existing_commands_response.json()
                 for command in existing_commands:
-                    command_id = command['id']
+                    command_id = command["id"]
                     delete_url = f"{commands_url}/{command_id}"
                     delete_response = rq.delete(delete_url, headers=headers)
                     if delete_response.status_code == 204:
-                        print(f"Deleted command {command['name']} from guild {guild_id}")
+                        print(f"Deleted command {command["name"]} from guild {guild_id}")
                     else:
-                        print(f"Failed to delete command {command['name']} from guild {guild_id}: {delete_response.text}")
+                        print(f"Failed to delete command {command["name"]} from guild {guild_id}: {delete_response.text}")
             else:
                 print(f"Failed to fetch existing commands for guild {guild_id}: {existing_commands_response.text}")
 
@@ -50,8 +53,10 @@ def lambda_handler(event, context):
             for command in command_list:
                 post_response = rq.post(commands_url, headers=headers, json=command)
                 if post_response.status_code == 201:
-                    print(f"Command {command['name']} successfully set for guild {guild_id}")
+                    print(f"Command {command["name"]} successfully set for guild {guild_id}")
                 else:
-                    print(f'Error {post_response.status_code}: {post_response.text}')
+                    print(f"Error {post_response.status_code}: {post_response.text}")
     else:
-        print(f'Error {response.status_code}: {response.text}')
+        print(f"Error {response.status_code}: {response.text}")
+
+lambda_handler("a", "a")
